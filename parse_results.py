feae228e742285fd17688899a7779e2cb76c174f
Ali Turfah <aturfah@gmail.com>
2020-12-11 16:06:09 -0500

    Ignore output files

--- parse_results.py
+++ parse_results.py
@@ -1,5 +1,5 @@
 from config import Config
-from misc import load_model_history
+from misc import load_model_history, clean_model_history, merge_results
 import pandas as pd
 from copy import deepcopy
 
@@ -16,14 +16,15 @@ def handle_fold_performance(fold_info):
             temp_loss.append(info[0])
             temp_acc.append(info[1])
             if len(epochs) < 100 / Config.validation_pct:
-                epochs.append(fold["time"])
+                epochs.append(fold["epochs"])
 
         output[output_key.format("{}", "loss")] = sum(temp_loss) / len(temp_loss)
         output[output_key.format("{}", "acc")] = sum(temp_acc) / len(temp_acc)
 
-    output["avg_time"] = sum(epochs) / len(epochs)
+    output["avg_epochs"] = sum(epochs) / len(epochs)
 
     return output
+
 
 def process_results_dictionary(res):
     output = {}
@@ -46,6 +47,7 @@ def process_results_dictionary(res):
 
     return output
 
+
 def prepare_df(df_in, prefix=None):
     output = deepcopy(df_in)
     # Rename some columns
@@ -62,6 +64,21 @@ def prepare_df(df_in, prefix=None):
     return output
 
 
+def prepare_dataset(list_of_performance, dataset_name):
+    dataset_label = Config.DATASET_NAME_MAP.get(dataset_name, dataset_name)
+
+    performance_df = pd.DataFrame(list_of_performance)
+    performance_df.drop(columns=["model_name"], inplace=True)
+
+    target_columns = list(performance_df.columns[0:4]) + ["avg_epochs"]
+    target_columns = target_columns + ["^{}.*".format(dataset_name)]
+
+    dataset_df = performance_df.filter(regex="|".join(target_columns))
+    dataset_df["dataset"] = [dataset_label] * len(list_of_performance)
+
+    return dataset_df
+
+
 def prepare_fashn(list_of_performance):
     performance_df = pd.DataFrame(list_of_performance)
     performance_df.drop(columns=["model_name"], inplace=True)
@@ -69,7 +86,7 @@ def prepare_fashn(list_of_performance):
     fashn_df = performance_df.filter(regex="|".join(list(performance_df.columns[0:4]) + ["avg_time"] + ["fashion.*"]))
     fashn_df = prepare_df(fashn_df, prefix="fashion_mnist_")
     fashn_df["dataset"] = ["Fashion MNIST"] * len(list_of_performance)
-    
+
     return fashn_df
 
 
@@ -82,6 +99,7 @@ def prepare_mnist(list_of_performance):
     mnist_df["dataset"] = ["MNIST"] * len(list_of_performance)
 
     return mnist_df
+
 
 def prepare_cifar10(list_of_performance):
     performance_df = pd.DataFrame(list_of_performance)
@@ -96,18 +114,29 @@ def prepare_cifar10(list_of_performance):
 
 if __name__ == "__main__":
     base_model_results = None
-    loop_cv_results = load_model_history()
+    loop_cv_results = clean_model_history(load_model_history())
+
     for old_fname in Config.old_results_fnames:
-        loop_cv_results.extend(load_model_history(old_fname))
+        loop_cv_results = merge_results(loop_cv_results,
+            clean_model_history(load_model_history(old_fname)))
 
     model_performance = []
 
     ## Get all model results in a Pandas DF
-    for res in loop_cv_results:
+    for key in loop_cv_results:
+        res = loop_cv_results[key]
         if res["model_name"] == "base_model":
+            if base_model_results:
+                raise RuntimeError("DUPLICATED BASE MODEL RESULTS")
             base_model_results = process_results_dictionary(res)
         else:
             model_performance.append(process_results_dictionary(res))
+
+    for dataset_name in Config.DATASETS:
+        print("\n\n{}".format(dataset_name))
+        prepare_dataset(model_performance, dataset_name)
+    
+    raise RuntimeError("DOOT DOOT")
 
     performance_df = pd.DataFrame(model_performance)
     performance_df.drop(columns=["model_name"], inplace=True)
@@ -116,11 +145,11 @@ if __name__ == "__main__":
     base_mnist = prepare_mnist([base_model_results])
     base_fashn = prepare_fashn([base_model_results])
 
-    raise RuntimeError("DOOT DOOT")
-
-    # Write to R
+    # Write for R analysis
     total_base = pd.concat([base_mnist, base_fashn])
     total_base.to_csv(Config.base_results_fname)
+
+
 
 
     ## Split across datasets & prepare them
