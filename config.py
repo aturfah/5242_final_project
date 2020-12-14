95ab1ce0ac6fd655b30e12aae67bdd2444607173
Ali Turfah <aturfah@gmail.com>
2020-12-13 20:10:03 -0500

    Shuffle around models and optimizers

--- config.py
+++ config.py
@@ -51,8 +51,8 @@ class Config():
     - g is a global averaging layer
     """
     model_arch = [
-        # "f|d:16|d:16|d:{prediction_head}", # Dumb linear models
-        "c:32:3:1|c:32:3:1|c:32:3:1|p:3|r|c:64:3:1|c:64:3:1|c:64:3:1|p:3|r|c:64:3:1|c:64:1:1|c:{prediction_head}:1:1|g", # ConvPool-CNN-C
+        "f|d:16|r|d:16|d:{prediction_head}", # Dumb linear models
+        # "c:32:3:1|c:32:3:1|c:32:3:1|p:3|r|c:64:3:1|c:64:3:1|c:64:3:1|p:3|r|c:64:3:1|c:64:1:1|c:{prediction_head}:1:1|g", # ConvPool-CNN-C
         # "c:32:3:1|c:32:3:1|p:3|r|c:64:3:1|c:64:3:1|p:3|r|c:64:3:1|c:64:1:1|c:{prediction_head}:1:1|g", # Model C
         # "c:32:3:1|c:32:3:2|r|c:64:3:1|c:64:3:2|r|c:64:3:1|c:64:1:1|c:{prediction_head}:1:1|g", # Strided CNN-C
         # "c:32:3:1|c:32:3:1|c:32:3:2|r|c:64:3:1|c:64:3:1|c:64:3:2|r|c:64:3:1|c:64:1:1|c:{prediction_head}:1:1|g", # All-CNN-C
@@ -77,8 +77,8 @@ class Config():
     # Optimizers for model fitting
     model_opt = [
         "adam", 
-        "adam5", # Adam starting LR is 10^-5
-        "adam4" # Adam starting LR is 10^-4
+        "swa", # FIGURE THIS OUT
+        "rectified_adam" # FIGURE THIS OUT
     ]
 
     ### Stuff for writing models to file
