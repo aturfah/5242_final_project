6b5763f1a4b0d645b5467d4a5237ae4abcb23445
Ali Turfah <aturfah@gmail.com>
2020-12-13 20:09:43 -0500

    Not sure what I changed here

--- generate_other_model_output.R
+++ generate_other_model_output.R
@@ -14,22 +14,40 @@ DATASETS = c("MNIST",
              "CIFAR-10",
              "Kuzushiji-49")
 data <- read_csv("proc_results.csv") %>%
-  select(-c("X1", "avg_epochs")) %>%
+  select(-c("X1")) %>%
   select(-starts_with("train"), -starts_with("valid"), -ends_with("loss")) %>%
+  select(-starts_with("med")) %>%
   mutate(dataset=factor(dataset, levels=DATASETS),
          architecture=factor(architecture,
                              levels=c("2FC",
                                       "Model C",
                                       "Strided CNN",
                                       "ConvPool CNN",
-                                      "All CNN"))) %>%
-                                      na.fail()
+                                      "All CNN"))) # %>% na.omit()
+
+temp_data <- data %>% 
+  filter(optimizer=="adam4") %>% 
+  arrange(desc(mean_test_acc))
+
+temp_data <- data %>%
+  filter(architecture != "2FC",
+         # dataset != "Kuzushiji-49",
+          initializer!="PEW",
+         min_test_acc < 0.15) %>%
+  arrange(initializer, desc(min_test_acc))
+
+temp_data %>% na.omit() %>%
+  # filter(dataset=="Fashion-MNIST") %>%
+  group_by(dataset) %>%
+  summarize(train_valid=mean(max_train_acc-max_valid_acc),
+            train_test=mean(max_train_acc-max_test_acc),
+            valid_test=mean(max_valid_acc-max_test_acc))
 
 performance_summary <- function(data) {
   summarize(data,
-            `Mean Test Acc.`=mean(test_acc),
+            `Mean Test Acc.`=mean(max_test_acc),
             `Mean Test Acc.`=round(`Mean Test Acc.`, 3),
-            `Med. Test Acc.`=median(test_acc),
+            `Med. Test Acc.`=median(max_test_acc),
             `Med. Test Acc.`=round(`Med. Test Acc.`, 3),
             ) %>%
     arrange(desc(`Mean Test Acc.`)) %>%
@@ -43,7 +61,7 @@ random_forest_results <- function(data, out_fname) {
     mutate(regularization=as.factor(regularization),
            initializer=as.factor(initializer),
            optimizer=as.factor(optimizer))  
-  data_rf <- randomForest(test_acc ~ .,
+  data_rf <- randomForest(max_test_acc ~ .,
                           mtry=2, ## HOW TO TUNE THIS
                           data = tree_data,
                           importance = TRUE)
@@ -73,7 +91,7 @@ random_forest_results <- function(data, out_fname) {
 
 
 # data %>%
-#   ggplot(aes(x=test_acc)) +
+#   ggplot(aes(x=max_test_acc)) +
 #   geom_histogram(fill="white", color="black", binwidth=0.03) +
 #   geom_density(alpha=0.8) +
 #   labs(title="", x="Test Accuracy", y="Count") +
@@ -82,7 +100,7 @@ random_forest_results <- function(data, out_fname) {
 
 png("images/architecture_comparison.png", height=800, width=800)
 data %>%
-  ggplot(aes(x=test_acc, fill=architecture)) +
+  ggplot(aes(x=max_test_acc, fill=architecture)) +
   geom_density(alpha=0.5, size=0.7) +
   scale_fill_hue(h=c(30, 330), l=70, direction=-1) +
   labs(title="", x="Test Accuracy", y="Density") +
@@ -96,7 +114,7 @@ dev.off()
 ### Just Optimizer
 png("images/optimizer_comparison.png", height=500, width=1000)
 data %>%
-  ggplot(aes(x=test_acc, fill=optimizer)) +
+  ggplot(aes(x=max_test_acc, fill=optimizer)) +
   geom_density(alpha=0.5, size=0.7) +
   scale_fill_hue(h=c(30, 330), l=70, direction=-1) +
   labs(title="", x="Test Accuracy", y="Density") +
@@ -108,7 +126,7 @@ dev.off()
 
 png("images/regularizer_comparison.png", height=500, width=1000)
 data %>%
-  ggplot(aes(x=test_acc, fill=regularization)) +
+  ggplot(aes(x=max_test_acc, fill=regularization)) +
   geom_density(alpha=0.5, size=0.7) +
   scale_fill_hue(h=c(30, 330), l=70, direction=-1) +
   labs(title="", x="Test Accuracy", y="Density") +
@@ -122,7 +140,7 @@ dev.off()
 ### Optimizer by Regularizer
 png("images/optimization_regularization_comparison.png", height=600, width=1000)
 data %>%
-  ggplot(aes(x=test_acc, fill=optimizer)) +
+  ggplot(aes(x=max_test_acc, fill=optimizer)) +
   geom_density(alpha=0.5, size=0.7) +
   scale_fill_hue(h=c(30, 330), l=70, direction=-1) +
   labs(title="", x="Test Accuracy",
@@ -136,7 +154,7 @@ dev.off()
 ### Optimizer by Architecture
 png("images/optimization_architecture_comparison.png", height=600, width=1000)
 data %>%
-  ggplot(aes(x=test_acc, fill=optimizer)) +
+  ggplot(aes(x=max_test_acc, fill=optimizer)) +
   geom_density(alpha=0.5, size=0.7) +
   scale_fill_hue(h=c(30, 330), l=70, direction=-1) +
   labs(title="", x="Test Accuracy",
@@ -149,7 +167,7 @@ dev.off()
 
 png("images/regularization_architecture_comparison.png", height=600, width=1000)
 data %>%
-  ggplot(aes(x=test_acc, fill=regularization)) +
+  ggplot(aes(x=max_test_acc, fill=regularization)) +
   geom_density(alpha=0.5, size=0.7) +
   scale_fill_hue(h=c(30, 330), l=70, direction=-1) +
   labs(title="", x="Test Accuracy",
@@ -170,7 +188,8 @@ for (dataset_name in DATASETS) {
   ## Best Performance
   cat(paste("\n\n\n", dataset_name, " Top Performers\n", sep=""))
   filt_data  %>%
-    arrange(desc(test_acc)) %>%
+    arrange(desc(max_test_acc)) %>%
+    select("architecture", "regularization", "initializer", "optimizer", ends_with("test_acc")) %>%
     head(8) %>%
     xtable(digits=3, floating=FALSE,latex.environments=NULL,booktabs=TRUE) %>%
     print(include.rownames=FALSE)
@@ -206,7 +225,9 @@ for (dataset_name in DATASETS) {
   ## RF. Analysis
   output_fname = paste("images/", dataset_name,
                        "_rf_variable_importance.png", sep="")
-  random_forest_results(filt_data, output_fname)
+  # random_forest_results(filt_data, output_fname)
 }
 rm(list=c("filt_data", "output_fname", "dataset_name"))
 
+
+temp_data <- 
