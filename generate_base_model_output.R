a931ef0ff80065b081e55a0b6d4a8e064c0a85f2
Ali Turfah <aturfah@gmail.com>
2020-12-16 17:43:17 -0500

    Fix this to work with new data format

--- generate_base_model_output.R
+++ generate_base_model_output.R
@@ -5,7 +5,7 @@ require(xtable)
 setwd("~/Documents/projects/5242_final_project/")
 ### Base Results
 base_data <- read_csv("base_results.csv") %>%
-  select("dataset", starts_with("train"), starts_with("valid"), starts_with("test")) %>%
+  select("dataset", starts_with("mean_train"), starts_with("mean_valid"), starts_with("mean_test")) %>%
   select("dataset", ends_with("acc"))
 
 base_data %>%
@@ -14,12 +14,24 @@ base_data %>%
 
 
 ### Generate training/validaiton convergence graph
+DATASETS = c("MNIST", "Fashion-MNIST", "Kuzushiji-MNIST",
+             "CIFAR-10", "Kuzushiji-49")
 dataset_from_filename <- function(fname) {
+  idx = 0
   if (str_detect(fname, "fashion_mnist")) {
-    return("Fashion MNIST")
+    idx = 1
+  } else if (str_detect(fname, "k49")) {
+    idx = 5
+  } else if (str_detect(fname, "kmnist")) {
+    idx = 3
+  } else if (str_detect(fname, "cifar10")) {
+    idx = 4
+  } else if (str_detect(fname, "mnist")) {
+    idx = 2
   } else {
-    return("MNIST")
+    stop("Invalid Dataset")
   }
+  return(DATASETS[idx])
 }
 
 eval_mode_from_filename <- function(fname) {
@@ -41,13 +53,13 @@ value_type_from_filename <- function (fname) {
 read_plus <- function(flnm) {
   read_csv(flnm) %>% 
     mutate(Dataset = dataset_from_filename(flnm),
-           Dataset = factor(Dataset, levels=c("MNIST", "Fashion MNIST")),
+           Dataset = factor(Dataset, levels=DATASETS),
            Mode = eval_mode_from_filename(flnm),
            Value_Type = value_type_from_filename(flnm),
            filename = flnm)
 }
 
-epoch_data <- list.files(path="base_model_results", pattern = "*csv", full.names=TRUE) %>% 
+epoch_data <- list.files(path="base_csv", pattern = "*csv", full.names=TRUE) %>% 
   map_df(~read_plus(.)) %>%
   group_by(Dataset, Mode, Value_Type, Step) %>%
   summarize(value = mean(Value))
@@ -55,7 +67,7 @@ epoch_data <- list.files(path="base_model_results", pattern = "*csv", full.names
 png("images/base_model_convergence.png", height=400, width=800)
 epoch_data %>%
   ggplot(aes(x=Step, y=value, color=Mode)) +
-  geom_line(alpha=0.5, size=1.5) +
+  geom_line(alpha=0.9, size=0.8) +
   labs(title="", x="Step", y="Value") +
   facet_grid(Value_Type ~ Dataset, scales="free_y") +
   theme_classic() +
