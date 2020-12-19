7f11674c46535864addbd941b8475e888ebb711d
Ali Turfah <aturfah@gmail.com>
2020-12-19 12:50:37 -0500

    Hahaha

--- combine_pickles.py
+++ combine_pickles.py
@@ -3,9 +3,11 @@ If some parts of the config changes & you don't want to waste
 time loading models just to have them be ignored later
 """
 from misc import load_model_history, save_model_history, clean_model_history, merge_results
+from misc import read_filenames_in_directory
+
 
 if __name__ == "__main__":
-    pickle_files = ["results_cv.pkl", "results_cv2.pkl", "results_cv3.pkl", "results_cv4.pkl", "results_cv5.pkl"]
+    pickle_files = read_filenames_in_directory("test_finished_pickles")
 
     base_data = {}
     for filename in pickle_files:
